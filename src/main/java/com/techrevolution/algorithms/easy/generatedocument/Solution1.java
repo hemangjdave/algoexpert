b3d312ebb94572b855df70b51a4a10719504e6f8
hemangjdave714 <hdave@mobiquityinc.com>
2021-11-30 16:49:41 +0530

    Added the logic for whether characters can generate the document or not. Added the test cases as well.

---
+++ src/main/java/com/techrevolution/algorithms/easy/generatedocument/Solution1.java
@@ -0,0 +1,31 @@
+package com.techrevolution.algorithms.easy.generatedocument;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created By Hemang Dave
+ * Date: 22/11/21
+ * Time: 3:49 PM
+ */
+@Slf4j
+public class Solution1 {
+    public static void main(String[] args) {
+        String characters = "aheaollabbhb";
+        String document = "hello";
+        log.info("Result is :-- {}", new Solution1().generateDocument(characters, document));
+    }
+
+    public boolean generateDocument(String characters, String document) {
+        // Write your code here.
+        if (document.length() == 0) {
+            return true;
+        }
+        char[] documentArray = document.toCharArray();
+        for (char ch : documentArray) {
+            var index = characters.indexOf(ch);
+            if (index == -1) return false;
+            characters = characters.replaceFirst(String.valueOf(ch), "");
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/techrevolution/algorithms/easy/generatedocument/Solution1Test.java
@@ -0,0 +1,22 @@
+package com.techrevolution.algorithms.easy.generatedocument;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Solution1Test {
+
+    private final Solution1 solution1 = new Solution1();
+
+    @Test
+    void testGenerateDocument() {
+        assertTrue(solution1.generateDocument("Bste!hetsi ogEAxpelrt x ", "AlgoExpert is the Best!"));
+        assertFalse(solution1.generateDocument("A", "a"));
+        assertTrue(solution1.generateDocument("a hsgalhsa sanbjksbdkjba kjx", ""));
+        assertFalse(solution1.generateDocument(" ", "hello"));
+        assertTrue(solution1.generateDocument("aheaollabbhb", "hello"));
+        assertTrue(solution1.generateDocument("&*&you^a%^&8766 _=-09     docanCMakemthisdocument", "Can you make this document &"));
+    }
+
+}
